9ae8298f2198214ab9717a98bf8edf45fe06419a
James Cogan <james.m.cogan@gmail.com>
2023-02-27 18:48:07 -0500

    cors

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
 
     validates :first_name, presence: true
     validates :last_name, presence: true
-    validates :email, presence: true, uniqeness: true
+    # validates :email, presence: true, uniqeness: true
     validates :password, presence: true
     validates :handle, presence: true
 end
