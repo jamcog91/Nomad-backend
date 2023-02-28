a90acaa3034ac90ffffe27c8a2b17368cb9d10dc
James Cogan <james.m.cogan@gmail.com>
2023-02-27 22:11:41 -0500

    login/out

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show, :create, :update, :destroy]
   
 
-  # post "/login" to: "sessions#create"
+  post "/login" to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end
