cecf80559353ca6d4ce993b954ad790d7942d293
James Cogan <james.m.cogan@gmail.com>
2023-03-01 20:23:15 -0500

    search

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   get "/login", to: "sessions#create"
   post "/signup", to: "users#create"
+  get "/me", to: "users#show"
   delete "/logout", to: "sessions#destroy"
+  get "/restaurant" to: "restaurant#index"
 end
