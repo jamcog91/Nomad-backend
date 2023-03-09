c623e5d00950549748f150644a933b57ffe2f314
James Cogan <james.m.cogan@gmail.com>
2023-03-08 23:04:06 -0500

    css

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -16,27 +16,26 @@ class ReviewsController < ApplicationController
     end
 
     def create
-        restaurant = Restaurant.find(params[:restaurant_id])
-        # byebug
-        score = params[:score]
-        user = User.find(params[:user_id])
+        # restaurant = Restaurant.find(params[:restaurant_id])
+        # # byebug
+        # score = params[:score]
+        # user = User.find(params[:user_id])
         new_review = Review.create!(review_params)
-        # byebug
-        if user.reviews.where(user_id: user.id).count >= 1
-            review = Review.where(user_id: user.id).limit(1).first
+        # if user.reviews.where(user_id: user.id).count >= 1
+        #     review = Review.where(user_id: user.id).limit(1).first
                 
-            restaurants = [review, new_review]
+        #     restaurants = [review, new_review]
 
-            ReviewComparisonPrompt.new(restaurants: restaurants).render
-            # restaurants = [
-            #     [Review.find_by(restaurant_id: review.restaurant_id)],
-            #     [restaurant_id, Review.find_by(restaurant_id: restaurant_Id)]
-            # ]
-            # if new_review.present?
-            #     increment_counter(:score, restaurant_id)
-            # end
-        end
-        render json: restaurants, status: :created
+        #     ReviewComparisonPrompt.new(restaurants: restaurants).render
+        #     # restaurants = [
+        #     #     [Review.find_by(restaurant_id: review.restaurant_id)],
+        #     #     [restaurant_id, Review.find_by(restaurant_id: restaurant_Id)]
+        #     # ]
+        #     # if new_review.present?
+        #     #     increment_counter(:score, restaurant_id)
+        #     # end
+        # end
+        render json: new_review, status: :created
     end
 
     def update
